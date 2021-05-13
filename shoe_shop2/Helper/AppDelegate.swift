4712a08161e79fc058e1aeabc21efc8f60d5b137
Nhat <>
2021-05-13 09:58:03 +0700

    fix import fireBase in appDelegate

--- shoe_shop2/Helper/AppDelegate.swift
+++ shoe_shop2/Helper/AppDelegate.swift
@@ -8,7 +8,7 @@
 import UIKit
 import CoreData
 import IQKeyboardManager
-
+import FirebaseCore
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         IQKeyboardManager.shared().isEnabled = true
+        FirebaseApp.configure()
         return true
     }
 
